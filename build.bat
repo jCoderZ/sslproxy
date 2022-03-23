68dcbab7242f94bdece67f4e2954159a919736fa
Christian Loroff <christian.loroff@t-systems.com>
2022-03-23 11:13:57 +0100

    switched to JDK 11

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @setlocal
 
-@echo adding JDK 1.5.0_05 to PATH
-@set PATH=%PATH%;c:\sdk\jdk1.5.0_15\bin
+@echo adding JDK to PATH
+@set PATH=%PATH%;D:\sdk\jdk-11.0.2\bin
 
 @echo compiling classes
 @javac -cp fawkez-all.jar org/jcoderz/keytoolz/sslproxy/*.java
